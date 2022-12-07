6baaa0bd51719dce82ff8d3b8ee9166ab14347a5
mr-brandt <martin.brandt@mabsoft-online.de>
2022-12-07 11:07:44 +0100

    Update index.tsx
    * Fixed Typescript warning "limit is possibly undefined"
    * Added option to have no limit at all by setting limit to -1

--- index.tsx
+++ index.tsx
@@ -1,32 +1,28 @@
 /* eslint-disable react/jsx-no-useless-fragment */
-import * as React from 'react';
+import * as React from "react";
 
 type WhenProps = {
-    children: React.ReactNode,
-    isTrue?: boolean,
-    limit?: number,
+    children: React.ReactNode;
+    isTrue?: boolean;
+    limit?: number;
 };
 
-const RenderWhen = ({ limit, isTrue, children }:WhenProps) => {
-    const list:React.ReactNode[] = [];
+const RenderWhen = ({ limit = 1, isTrue, children }: WhenProps) => {
+    const list: React.ReactNode[] = [];
 
     if (isTrue !== true) {
         return null;
     }
 
-    React.Children.map(children, (child:any) => {
+    React.Children.map(children, (child: any) => {
         const { isTrue: isChildTrue } = child?.props || {};
 
-        if (isChildTrue === true && list.length < limit) {
+        if (isChildTrue === true && (limit == -1 || list.length < limit)) {
             list.push(child);
         }
     });
 
-    return (
-        <>
-            {list}
-        </>
-    );
+    return <>{list}</>;
 };
 
 RenderWhen.defaultProps = {
